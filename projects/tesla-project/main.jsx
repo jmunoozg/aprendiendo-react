8267fdbbe65a59279405daefceafb59692a8ffaa
Jaime Muñoz <jaime.munozgab@gmail.com>
2025-02-04 13:47:55 +0100

    primera parte pag web

---
+++ projects/tesla-project/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <p>Hola Mundo</p>
+  </React.StrictMode>
+);
